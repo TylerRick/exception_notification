e378152160f4751fb0e55b7ee5fab5d1a0aacabc
Tyler Rick <github.com@tylerrick.com>
2010-06-16 22:28:09 -0700

    Made it so that you could send notifications without raising an exception.
    So now you can do something like:
      begin
        something_dangerous
      rescue
        notify_of $!
        clean_up_after_error_and_continue
      end
    
    You will still be notified of the error but without disrupting the user's
    experience.  Just because you rescue your exceptions instead of letting them
    blow up on the user doesn't mean you don't want to find out about them so you
    can prevent them from happening in the future!
    
    Whenever you have a situation that is exceptional (uncommon) but you want to
    simply handle the situation instead of raising the exception, causing a 500
    error, and showing an error to your user, you can use notify_of.
    
    The ExceptionNotifier.notify_of method also lets you send notifications of
    events other than Exceptions. Just pass :message => 'your custom message'
    instead of passing an exception object.
    
    Changed exception_notification method to be more generic (accepts a hash now
    instead of exception, controller, and request).
    
    It attempts to normalize things so you still have access to most of the same
    data no matter what you passed to notify_of/exception_notification.
    
    Added some documentation.
    
    Changed some of the message sections.

--- lib/exception_notifiable.rb
+++ lib/exception_notifiable.rb
@@ -39,6 +39,9 @@ module ExceptionNotifiable
       addresses
     end
 
+    # Pass a symbol to specify a method in your controller (likely ApplicationController) that will return a hash of additional data to be available to the ExceptionNotifier views
+    # Pass a proc if you have a proc that will return the extra data.
+    # I'm not sure what happens if you pass self?
     def exception_data(deliverer=self)
       if deliverer == self
         read_inheritable_attribute(:exception_data)
@@ -56,12 +59,33 @@ module ExceptionNotifiable
     end
   end
 
+  public
+
+    # You can send a notice manually, even without an exception notice. Just pass a hash with these keys:
+    # * error_class (optional)
+    # * message
+    def notify_of(hash_or_exception)
+      notice = normalize_notice(hash_or_exception)
+
+      source_of_extra_data = self.class.exception_data
+      extra_data = case source_of_extra_data
+        when nil then {}
+        when Symbol then send(source_of_extra_data)
+        when Proc then source_of_extra_data.call(self)
+      end
+      notice.merge!(extra_data)
+
+      ExceptionNotifier.deliver_exception_notification(notice)
+    end
+
   private
 
     def local_request?
+      #Rails.logger.debug "ExceptionNotifiable: local_request?"
       remote = IPAddr.new(request.remote_ip)
       !self.class.local_addresses.detect { |addr| addr.include?(remote) }.nil?
     end
+
 
     def render_404
       respond_to do |type|
@@ -77,23 +101,87 @@ module ExceptionNotifiable
       end
     end
 
+    def normalize_notice(notice) #:nodoc:
+      data = {
+        :environment => ENV.to_hash,
+      }
+
+      if self.respond_to?(:controller_name) && self.respond_to?(:action_name)
+        data[:location] = "#{controller_name}##{action_name}"
+        data[:controller] =self
+      end
+
+      if self.respond_to? :request
+        data.merge!({
+          :request        => request,
+          :remote_address => (request.env["HTTP_X_FORWARDED_HOST"] || request.env["HTTP_HOST"]),
+          :rails_root     => rails_root,
+          :params         => request.parameters.to_hash,
+          :url            => "#{request.protocol}#{request.host}#{request.request_uri}",
+        })
+        data[:environment].merge!(request.env.to_hash)
+      end
+
+      if self.respond_to? :session
+        data.merge!({
+          :session      => session,
+          :session_id   => session.instance_variable_get("@session_id"),
+          :session_data => session.respond_to?(:to_hash) ?
+                           session.to_hash :
+                           session.instance_variable_get("@data")
+        })
+      end
+
+      case notice
+      when Hash
+        data.merge!({
+          :location    => sanitize_backtrace(caller)[1],
+          :backtrace   => sanitize_backtrace(caller),
+          :error_class => notice[:message],
+        })
+        data.merge!(notice)
+      when Exception
+        data.merge!(exception_to_data(notice))
+      end
+      data
+    end
+
+    def exception_to_data(exception)
+      data = {
+        :exception     => exception,
+        :error_class   => exception.class.name,
+        :message       => exception.message,
+        :backtrace     => sanitize_backtrace(exception.backtrace),
+      }
+      data
+    end
+
     def rescue_action_in_public(exception)
       case exception
         when *self.class.exceptions_to_treat_as_404
           render_404
 
-        else          
+        else
           render_500
-
-          deliverer = self.class.exception_data
-          data = case deliverer
-            when nil then {}
-            when Symbol then send(deliverer)
-            when Proc then deliverer.call(self)
-          end
-
-          ExceptionNotifier.deliver_exception_notification(exception, self,
-            request, data)
+          notify_of exception
       end
     end
+
+    def sanitize_backtrace(trace)
+      re = Regexp.new(/^#{Regexp.escape(rails_root)}\//)
+      trace.map { |line| Pathname.new(line.gsub(re, "")).cleanpath.to_s }
+    end
+
+    def rails_root
+      @rails_root ||= Pathname.new(RAILS_ROOT).cleanpath.to_s
+    end
+
+
+  # Since we can't instantiate ExceptionNotifiable directly (since it's a module), we need a class to put the ExceptionNotifiable methods to allow users to send notifications manually outside of a controller (normally the controller class is the class we use).
+  class FakeController
+#    def rescue_action_in_public(exception)
+#    end
+
+    include ExceptionNotifiable
+  end
 end
--- lib/exception_notifier.rb
+++ lib/exception_notifier.rb
@@ -37,30 +37,21 @@ class ExceptionNotifier < ActionMailer::Base
 
   def self.reloadable?() false end
 
-  def exception_notification(exception, controller, request, data={})
+  def exception_notification(notice={})
     content_type "text/plain"
 
-    subject    "#{email_prefix}#{controller.controller_name}##{controller.action_name} (#{exception.class}) #{exception.message.inspect}"
+    subject    "#{email_prefix}in #{notice[:location]}: #{notice[:error_class]}: #{notice[:message]}"
 
     recipients exception_recipients
     from       sender_address
 
-    body       data.merge({ :controller => controller, :request => request,
-                  :exception => exception, :host => (request.env["HTTP_X_FORWARDED_HOST"] || request.env["HTTP_HOST"]),
-                  :backtrace => sanitize_backtrace(exception.backtrace),
-                  :rails_root => rails_root, :data => data,
-                  :sections => sections })
+    body       notice.merge({
+                  :sections => sections 
+               })
   end
 
-  private
-
-    def sanitize_backtrace(trace)
-      re = Regexp.new(/^#{Regexp.escape(rails_root)}/)
-      trace.map { |line| Pathname.new(line.gsub(re, "[RAILS_ROOT]")).cleanpath.to_s }
-    end
-
-    def rails_root
-      @rails_root ||= Pathname.new(RAILS_ROOT).cleanpath.to_s
-    end
+  def self.notify_of(*args)
+    ExceptionNotifiable::FakeController.new.notify_of(*args)
+  end
 
 end
